b69d680e2bcc0ce0bd9e05ecc675a4cb63f4c5e1
zhangjia <zhangjia5551@126.com>
2021-01-06 22:33:17 +0800

    a

--- 尚硅谷/chapter11/encapexercise/main/main.go
+++ 尚硅谷/chapter11/encapexercise/main/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"go_code/chapter11/encapexercise/model"
+	"go/尚硅谷/chapter11/encapexercise/model"
 )
 
 func main() {
 	//创建一个account变量
-	account := model.NewAccount("jzh11111", "000", 40)
+	account := model.NewAccount("jzhgo/11111", "000", 40)
 	if account != nil {
 		fmt.Println("创建成功=", account)
 	} else {
--- 尚硅谷/chapter11/exercise/main.go
+++ 尚硅谷/chapter11/exercise/main.go
@@ -1,20 +1,30 @@
 package main
+
 import "fmt"
+
+// Usb 定义接口
 type Usb interface {
 	Say()
 }
+
+// Stu 定义结构体
 type Stu struct {
 }
-func (this *Stu) Say() {
+
+// Say 实现接口方法
+func (s *Stu) Say() { // *Stu指针实现的Usb接口的Say()方法，但Stu并没有实现
 	fmt.Println("Say()")
 }
-func main() {
-	var stu Stu = Stu{}
-	// 错误！ 会报 Stu类型没有实现Usb接口 , 
-	// 如果希望通过编译,  var u Usb = &stu
-	var u Usb = stu  
-	u.Say()
-	fmt.Println("here", u)
-}
 
-	
+func main() {
+
+	var stu Stu = Stu{}
+	fmt.Println(stu)
+	// 错误！ 会报 Stu类型没有实现Usb接口 ,
+	// 如果希望通过编译,  var u Usb = &stu
+	/*
+		var u Usb = stu // 会报 Stu类型没有实现Usb接口 ,
+		u.Say()
+		fmt.Println("here", u)
+	*/
+}
