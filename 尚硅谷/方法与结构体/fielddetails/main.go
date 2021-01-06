bb6bdffb8f36c57724a782066a72f10bfdebaf41
zhangjia <zhangjia5551@126.com>
2021-01-06 21:25:03 +0800

    a

--- 尚硅谷/方法与结构体/fielddetails/main.go
+++ 尚硅谷/方法与结构体/fielddetails/main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 )
@@ -6,20 +7,21 @@ import (
 //如果结构体的字段类型是: 指针，slice，和map的零值都是 nil ，即还没有分配空间
 //如果需要使用这样的字段，需要先make，才能使用.
 
-type Person struct{
-	Name string
-	Age int
+// Person 定义结构体
+type Person struct {
+	Name   string
+	Age    int
 	Scores [5]float64
-	ptr *int //指针 
-	slice []int //切片
-	map1 map[string]string //map
+	ptr    *int              //指针
+	slice  []int             //切片
+	map1   map[string]string //map
 }
 
-type Monster struct{
+// Monster 定义结构体
+type Monster struct {
 	Name string
-	Age int
+	Age  int
 }
-
 
 func main() {
 
@@ -27,37 +29,38 @@ func main() {
 	var p1 Person
 	fmt.Println(p1)
 
-	if p1.ptr == nil {
-		fmt.Println("ok1")
+	if p1.ptr == nil { // 指针类型的初始值为nil
+		fmt.Println("ok1") // ok1
 	}
 
-	if p1.slice == nil {
-		fmt.Println("ok2")
+	if p1.slice == nil { // 切片为指针类型，初始值为nil
+		fmt.Println("ok2") // ok1
 	}
+	fmt.Println(p1.slice)
 
 	if p1.map1 == nil {
 		fmt.Println("ok3")
 	}
 
-	//使用slice, 再次说明，一定要make
-	p1.slice = make([]int, 10)
-	p1.slice[0] = 100 //ok
+	// //使用slice, 再次说明，一定要make
+	// p1.slice = make([]int, 10)
+	// p1.slice[0] = 100 //ok
 
-	//使用map, 一定要先make
-	p1.map1 = make(map[string]string)
-	p1.map1["key1"] = "tom~" 
-	fmt.Println(p1)
+	// //使用map, 一定要先make
+	// p1.map1 = make(map[string]string)
+	// p1.map1["key1"] = "tom~"
+	// fmt.Println(p1)
 
-	//不同结构体变量的字段是独立，互不影响，一个结构体变量字段的更改，
-	//不影响另外一个, 结构体是值类型
-	var monster1 Monster
-	monster1.Name = "牛魔王"
-	monster1.Age = 500
+	// //不同结构体变量的字段是独立，互不影响，一个结构体变量字段的更改，
+	// //不影响另外一个, 结构体是值类型
+	// var monster1 Monster
+	// monster1.Name = "牛魔王"
+	// monster1.Age = 500
 
-	monster2 := monster1 //结构体是值类型，默认为值拷贝
-	monster2.Name = "青牛精"
+	// monster2 := monster1 //结构体是值类型，默认为值拷贝
+	// monster2.Name = "青牛精"
 
-	fmt.Println("monster1=", monster1) //monster1= {牛魔王 500}
-	fmt.Println("monster2=", monster2) //monster2= {青牛精 500}
+	// fmt.Println("monster1=", monster1) //monster1= {牛魔王 500}
+	// fmt.Println("monster2=", monster2) //monster2= {青牛精 500}
 
 }
--- 尚硅谷/方法与结构体/结构体指针的理解/main.go
+++ 尚硅谷/方法与结构体/结构体指针的理解/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Person 定义结构体
 type Person struct {
 	Name string
 	Age  int
@@ -14,10 +15,10 @@ func main() {
 	var p1 Person
 	p1.Age = 10
 	p1.Name = "小明"
-	var p2 *Person = &p1 //这里是关键-->画出示意图
+	var p2 *Person = &p1 // 这里是关键-->画出示意图
 
-	fmt.Println((*p2).Age)
-	fmt.Println(p2.Age)
+	fmt.Println((*p2).Age) // 10 底层已经把(*p2).Age与p2.Age等价了
+	fmt.Println(p2.Age)    // 10
 	p2.Name = "tom~"
 	fmt.Printf("p2.Name=%v p1.Name=%v \n", p2.Name, p1.Name)    // tom~ tom~
 	fmt.Printf("p2.Name=%v p1.Name=%v \n", (*p2).Name, p1.Name) // tom~ tom~
