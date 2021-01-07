d0e2f90735f7853b36752f9e6e7920dbf51a1820
zhangjia <zhangjia5551@126.com>
2021-01-07 21:49:18 +0800

    a

--- MyGo/随机输出切片中的元素/main.go
+++ MyGo/随机输出切片中的元素/main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-
 func main() {
-    rand.Seed(time.Now().UnixNano())
-    slice := []string{
-        "aa bb cc",
-        "bbbbb",
-        "ccccc",
-        "ddddd",
-        "eee",
-        "ff",
+	rand.Seed(time.Now().UnixNano())
+	slice := []string{
+		"aa bb cc",
+		"bbbbb",
+		"ccccc",
+		"ddddd",
+		"eee",
+		"ff",
 		"hh",
-    }
+	}
 	for {
-    	str := slice[rand.Intn(len(slice))]
-    	fmt.Println(str)
+		str := slice[rand.Intn(len(slice))]
+		fmt.Println(str)
 		time.Sleep(time.Second / 2)
 	}
 }
-
